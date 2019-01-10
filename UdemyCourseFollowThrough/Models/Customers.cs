c174ef9f2bcd7afe3f57de520862a7b2e0219e05
Harrysau <harrysauchhinsun@ymail.com>
2019-01-10 17:43:27 +0700

    Add Movies and Customers model and update database

---
+++ UdemyCourseFollowThrough/Models/Customers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UdemyCourseFollowThrough.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string cusName { get; set; }
+    }
+}
---
+++ UdemyCourseFollowThrough/Models/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UdemyCourseFollowThrough.Models
+{
+    public class Movies
+    {
+        [Key]
+        public string MovieId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
