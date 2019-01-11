713b6838267387dba6583f9a77930a8db2bb90f4
Harry <harrysauchhinsun@ymail.com>
2019-01-11 20:56:49 +0700

    rename HarryLaptop to HarryLaptopContext

---
+++ UdemyCourseFollowThrough/Models/HarryDesktopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UdemyCourseFollowThrough.Models
+{
+    public class HarryDesktopContext : DbContext
+    {
+        public HarryDesktopContext()
+            : base("name=DefaultConnection1")
+        {
+        }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ UdemyCourseFollowThrough/Models/HarryLaptopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UdemyCourseFollowThrough.Models
+{
+    public class HarryLaptopContext : DbContext
+    {
+        public HarryLaptopContext() 
+            :base("name=DefaultConnection")
+        {
+        }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
